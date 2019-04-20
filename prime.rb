d54b30672c22d35c854956778e7ed995d29a6677
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-20 23:16:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,12 @@
 def prime?(integer)
   if integer <= 1
     return false
-    elsif integer > 1
-      (2...integer).each do |i|
-        if (integer % i).zero?
-          return false
-        end
-  end
+      elsif integer > 1
+        (2...integer).each do |i|
+          if (integer % i).zero?
+            return false
+          end
+      end
   end
   return true  
 end
